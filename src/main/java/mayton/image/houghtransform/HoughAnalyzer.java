760580693f9499a052e86ead583b876e4dc8f7fc
mayton <mark.kovalyov78@gmail.com>
2020-05-15 22:34:37 +0300

    Changes in Hough Transform

--- src/main/java/mayton/image/houghtransform/HoughAnalyzer.java
+++ src/main/java/mayton/image/houghtransform/HoughAnalyzer.java
@@ -1,30 +1,50 @@
 package mayton.image.houghtransform;
 
 import mayton.image.Raster;
-import mayton.image.Rect;
 import mayton.math.IMatrix;
 import mayton.math.ISize;
 import mayton.math.Matrix;
-import mayton.math.Utils;
-import org.apache.commons.lang3.tuple.Pair;
+import org.apache.commons.lang3.tuple.Triple;
 
 import java.awt.image.BufferedImage;
+import java.util.Properties;
 import java.util.function.Function;
 
-public class HoughAnalyzer implements Function<Pair<BufferedImage, ISize>, IMatrix> {
+/**
+ * Преобразование Хафа
+ *
+ * x = [0..π]
+ * y = [0..R]
+ *
+ *
+ */
+public class HoughAnalyzer implements Function<Triple<BufferedImage, ISize, Properties>, IMatrix> {
+
+    private static final double π = Math.PI;
+
+    public int roundLineWithHoughFilling(int x, int y) {
+        double θ = π / 32.0;
+        for(double φ = 0.0; φ < π ; φ += θ) {
+
+        }
+        return 0;
+    }
 
     @Override
-    public IMatrix apply(Pair<BufferedImage, ISize> bufferedImageRectPair) {
+    public IMatrix apply(Triple<BufferedImage, ISize, Properties> bufferedImageRectPair) {
         IMatrix matrix = new Matrix();
+        Properties props = bufferedImageRectPair.getRight();
         BufferedImage image = bufferedImageRectPair.getLeft();
-        ISize size = bufferedImageRectPair.getRight();
+        ISize size = bufferedImageRectPair.getMiddle();
         int w = size.getX();
         int h = size.getY();
-        for(int x = 0;x<w;x++) {
-            for(int y = 0;y<h;y++) {
-                int color = image.getRGB(x,y);
-                boolean isWhite = Raster.isPixelWhite(color);
 
+        //
+        for (int x = 0; x < w; x++) {
+            for (int y = 0; y < h; y++) {
+                int color = image.getRGB(x, y);
+                boolean isWhite = Raster.isPixelWhite(color);
+                roundLineWithHoughFilling(x, y);
             }
         }
 
