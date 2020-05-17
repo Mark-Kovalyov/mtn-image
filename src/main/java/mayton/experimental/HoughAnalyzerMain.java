a2807df9632960b99786c7131e422589c01c560c
mayton <mark.kovalyov78@gmail.com>
2020-05-17 20:19:08 +0300

    Improove Hough

--- src/main/java/mayton/experimental/HoughAnalyzerMain.java
+++ src/main/java/mayton/experimental/HoughAnalyzerMain.java
@@ -1,26 +1,37 @@
 package mayton.experimental;
 
+import mayton.image.Rect;
 import mayton.image.houghtransform.HoughAnalyzer;
-import mayton.math.IMatrix;
 import org.apache.commons.lang3.tuple.ImmutablePair;
-import org.apache.commons.lang3.tuple.ImmutableTriple;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Properties;
 
 public class HoughAnalyzerMain {
 
+    public static void drawTestImage() {
+        BufferedImage i = new BufferedImage(512,512,BufferedImage.TYPE_INT_ARGB);
+        ImageIO.write(result, "PNG", new FileOutputStream("/documents/image-processing/Color.Images/test-img.png"));
+    }
+
     public static void main(String[] args) throws IOException {
         HoughAnalyzer houghAnalyzer = new HoughAnalyzer();
+        Rect inputRect = new Rect(0, 0, 128, 128);
+        Rect houghRect = new Rect(0, 0, 512, 512);
+        Properties properties = new Properties();
+        properties.put("inputRect", inputRect);
+        properties.put("houghRect", houghRect);
         BufferedImage result = houghAnalyzer.apply(
                 ImmutablePair.of(
                         ImageIO.read(new FileInputStream("/documents/image-processing/Color.Images/lena.ppm.png")),
-                        new Properties()
+                        properties
                 ));
-        
+
+        ImageIO.write(result, "PNG", new FileOutputStream("/documents/image-processing/Color.Images/lena-hough.png"));
 
     }
 
--- src/main/java/mayton/image/houghtransform/HoughAnalyzer.java
+++ src/main/java/mayton/image/houghtransform/HoughAnalyzer.java
@@ -1,29 +1,56 @@
 package mayton.image.houghtransform;
 
 import mayton.image.Raster;
-import mayton.math.IMatrix;
-import mayton.math.ISize;
-import mayton.math.Matrix;
+import mayton.image.Rect;
 import org.apache.commons.lang3.tuple.Pair;
-import org.apache.commons.lang3.tuple.Triple;
+import org.jetbrains.annotations.NotNull;
 
+import javax.annotation.concurrent.ThreadSafe;
 import java.awt.image.BufferedImage;
 import java.util.Properties;
 import java.util.function.Function;
+
+import static java.lang.Math.sin;
+import static java.lang.Math.sqrt;
+
 
 /**
  * Преобразование Хафа
  *
  * x = [0..π]
- * y = [0..R]
+ * y = [-R..R]
  *
- * Уравнение прямой
+ * Уравнение прямой в полярных координатах
  *
  *
  */
+@ThreadSafe
 public class HoughAnalyzer implements Function<Pair<BufferedImage, Properties>, BufferedImage> {
 
     private static final double π = Math.PI;
+
+    private double R = 0.0;
+
+    /**
+     * Draws sine function
+     */
+    public void dropSine(BufferedImage houghPlane, Rect sourceRect, int xx, int yy) {
+        double ρ;
+        double φ;
+        double radius;
+        int h = houghPlane.getHeight();
+        int w = houghPlane.getWidth();
+        for(int x = 0; x < w; x++) {
+            int y = (int) (120 + 120.0 * sin(x / 200.0));
+            houghPlane.setRGB(x, y, 0xFF_FFFFFF);
+        }
+    }
+
+    public double detectRadius(Rect rect) {
+        int height = rect.getHeight();
+        int width = rect.getWidth();
+        return sqrt((double) width * width + (double) height * height);
+    }
 
     public int roundLineWithHoughFilling(int x, int y) {
         double θ = π / 32.0;
@@ -33,26 +60,38 @@ public class HoughAnalyzer implements Function<Pair<BufferedImage, Properties>, 
         return 0;
     }
 
+    /**
+     *
+     * @param bufferedImageRectPair
+     * @return houghPlane
+     */
     @Override
-    public BufferedImage apply(Pair<BufferedImage, Properties> bufferedImageRectPair) {
-        IMatrix matrix = new Matrix();
+    public BufferedImage apply(@NotNull Pair<BufferedImage, Properties> bufferedImageRectPair) {
         BufferedImage image = bufferedImageRectPair.getLeft();
-        BufferedImage out = new BufferedImage(512, 512, BufferedImage.TYPE_BYTE_GRAY);
         Properties props = bufferedImageRectPair.getRight();
-
+        // For parallel processing
+        Rect inputRect = (Rect) props.get("inputRect");
         //ISize size = bufferedImageRectPair.getMiddle();
-        int w = image.getWidth();
-        int h = image.getHeight();
+        int x1 = inputRect.x1;
+        int y1 = inputRect.y1;
+        int x2 = inputRect.x2;
+        int y2 = inputRect.y2;
+
+        Rect houghRect = (Rect) props.get("houghRect");
+
+        BufferedImage houghPlane = new BufferedImage(houghRect.getWidth(), houghRect.getHeight(), BufferedImage.TYPE_BYTE_GRAY);
+
+        R = detectRadius(houghRect);
 
         //
-        for (int x = 0; x < w; x++) {
-            for (int y = 0; y < h; y++) {
+        for (int x = x1; x < x2; x++) {
+            for (int y = y1; y < y2; y++) {
                 int color = image.getRGB(x, y);
                 boolean isWhite = Raster.isPixelWhite(color);
-                roundLineWithHoughFilling(x, y);
+                dropSine(houghPlane, inputRect, x, y);
             }
         }
 
-        return out;
+        return houghPlane;
     }
 }
